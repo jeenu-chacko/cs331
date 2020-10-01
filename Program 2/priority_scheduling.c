14c36ff17f4bd2c5cb18efd2116e830e4e0d0900
jeenu-chacko <jeenu.chacko7@gmail.com>
2020-10-02 00:51:55 +0530

    added program 2

--- Program 2/priority_scheduling.c
+++ Program 2/priority_scheduling.c
@@ -4,7 +4,7 @@
  * 
  * cs331 System Software Lab 
  * 
- * Implementation of Non-Pre-Emptive First Come First Serve (FCFS)
+ * Implementation of priority scheduling algorithm
  * CPU Scheduling Algorithm for finding turnaround time and waiting time.
  *  
  */
@@ -21,6 +21,7 @@ struct process
  int c_time;
  float tat;
  float wt;
+ int pr;
 }p[20],temp,wt;
 
 
@@ -39,6 +40,8 @@ float avg_wt=0,avg_tat=0;
 	scanf("%d",&p[i].arrival_time);
 	printf("Enter the process burst time (ms) ");
 	scanf("%d",&p[i].burst_time);
+	printf("Enter the process priority(eg:priority 1>priority 2) ");
+	scanf("%d",&p[i].pr);
 	p[i].c_time=0;
 	
 	}
@@ -71,32 +74,7 @@ float avg_wt=0,avg_tat=0;
 
 
 
-	for(i=1;i<n-1;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(p[i].burst_time > p[j].burst_time)
-			{
-			temp = p[j];
-			p[j] = p[i];
-			p[i] = temp;
-			}	
-			else if(p[i].burst_time==p[j].burst_time)
-			{
-				if(p[i].arrival_time > p[j].arrival_time)
-				{
-					temp = p[j];
-					p[j] = p[i];
-					p[i] = temp;
-				}
-				
-				
-
-
-
-			}
-		}
-	}
+	
 
 	
 
@@ -114,57 +92,69 @@ float avg_wt=0,avg_tat=0;
 		{
 		p[i].c_time=p[i].burst_time+p[i].arrival_time;
 		}	
-		else if((p[i-1].c_time>=p[i].arrival_time)&&(p[i].c_time==0))
-		{
-		p[i].c_time=p[i-1].c_time+p[i].burst_time;
-		}
+		
 		else
 		{
-		for(k=i;k<n;k++)
-		{
-		if((p[i-1].c_time>=p[k].arrival_time)&&(p[i].c_time==0)&&(flag==0))
-		{
-			p[k].c_time=p[i-1].c_time+p[k].burst_time;		
-			flag=1;
-			temp=p[i];							
-			p[i]=p[k];									
-			p[k]=temp;
-
-			for(x=i+1;x<n-1;x++)
-			{
-			for(z=i+2;z<n;z++)
-			{	
-
-				if(p[x].burst_time > p[z].burst_time)							
-												
-				{
-				temp = p[x];
-				p[x] = p[z];
-				p[z] = temp;
-				}	
-				else if(p[x].burst_time==p[z].burst_time)
-				{
-				if(p[x].arrival_time > p[z].arrival_time)
-				{
-				temp = p[x];
-				p[x] = p[z];
-				p[z] = temp;
-				}
-				}	
-			 }
-			 }
-		}
-				
 		
+
+
+
+
+	for(k=i;k<n-1;k++)
+	{
+		
+		for(j=i+1;j<n;j++)
+		{
+			if((p[k].pr > p[j].pr) && (p[k].arrival_time <= p[i-1].c_time) && (p[j].arrival_time <= p[i-1].c_time) && (p[k].c_time==0) && (p[j].c_time==0))
+			{
+			temp = p[j];
+			p[j] = p[k];
+			p[k] = temp;
+			
+			}
+			if((p[k].pr==p[j].pr) && (p[k].arrival_time <= p[i-1].c_time) && (p[j].arrival_time <= p[i-1].c_time) && (p[k].c_time==0) && (p[j].c_time==0))
+			{
+				if(p[k].arrival_time>p[j].arrival_time)
+					{
+					temp = p[j];
+					p[j] = p[k];
+					p[k] = temp;
+					}				
+				
+			
+			}
+				
+			
+			
 		}
+	}
+
+
+
+
+
+
+
+		p[i].c_time=p[i-1].c_time+p[i].burst_time;
+
+
 
 		}
+
+
+
 	
 	}	
 
 
+	for(i=0;i<n;i++)
+	{	
+	printf("%d %d\n",p[i].pin,p[i].c_time);
+	}
+	
+
+
 	printf("\n\n");
-	printf("Shortest Job First\n\n");
 	printf("\tProcess\t\tTurn around time\tWaiting Time\n\n");
 
 	for(i=0;i<n;i++)
--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 1. Program 1
   	-	**fcfs.c** : first come,first served  non-preemptive CPU sheduling  
 	- **sjf.c** : shortest job first non-preemptive CPU sheduling  
+1. Program 2
+  	-	**priority_scheduling.c** : priority scheduling algorithm non-preemptive  
+	- **round-robin.c** : Round robin algoritm preemptive  
